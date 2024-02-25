7445920b8ad75b05ca638cfc263f947b351b72e3
d00161 <dheerajsingh@16199>
2024-02-25 19:56:17 +0530

    History feature implemented for tic tac toe game

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Square } from './Square';
 import { Board } from './Board';
+import { Game } from './Game';
 
 function App() {
   return (
     <div className="App">
-      <Board/>
+      <Game/>
 
     </div>
   );
--- src/Board.tsx
+++ src/Board.tsx
@@ -5,20 +5,17 @@ import { IndexType } from "typescript";
 
 
 interface BoardProps {
-    value: string;
-    onSquareClick: () => void;
+    xIsNext: boolean;
+    squares: string[];
+    onPlay: (nextSquares: string[]) => void;
   }
   
 
-export const Board =  ()=> {
-
-    const [xIsNext, setXIsNext] = useState<boolean>(true);
-
-    const [squares, setSquares] = useState(Array(9).fill(null));
+export const Board =  ({xIsNext, squares, onPlay}: BoardProps)=> {
 
     let status;
 
-    const calculateWinner = (squares: number[]) => {
+    const calculateWinner = (squares: string[]) => {
         const lines: number[][] = [
             [0,1,2],
             [3,4,5],
@@ -50,9 +47,7 @@ export const Board =  ()=> {
         }else{
             nextSquares[i]="O";
         }
-        console.log(nextSquares);
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares)
     }
 
 
@@ -69,19 +64,19 @@ export const Board =  ()=> {
         <div className="status">{status}</div>
 
         <div className="board-row">
-            <Square value={squares[0]} onSquareClick = {() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick = {() => handleClick(1)} />
-            <Square value={squares[2]} onSquareClick = {() => handleClick(2)} />
+            <Square key={0} value={squares[0]} onSquareClick = {() => handleClick(0)} />
+            <Square key={1} value={squares[1]} onSquareClick = {() => handleClick(1)} />
+            <Square key={2} value={squares[2]} onSquareClick = {() => handleClick(2)} />
         </div>
         <div className="board-row">
-            <Square value={squares[3]} onSquareClick = {() => handleClick(3)} />
-            <Square value={squares[4]} onSquareClick = {() => handleClick(4)} />
-            <Square value={squares[5]} onSquareClick = {() => handleClick(5)} />
+            <Square key={3} value={squares[3]} onSquareClick = {() => handleClick(3)} />
+            <Square key={4} value={squares[4]} onSquareClick = {() => handleClick(4)} />
+            <Square key={5} value={squares[5]} onSquareClick = {() => handleClick(5)} />
         </div>
         <div className="board-row">
-            <Square value={squares[6]} onSquareClick = {() => handleClick(6)} />
-            <Square value={squares[7]} onSquareClick = {() => handleClick(7)} />
-            <Square value={squares[8]} onSquareClick = {() => handleClick(8)} />
+            <Square key={6} value={squares[6]} onSquareClick = {() => handleClick(6)} />
+            <Square key={7} value={squares[7]} onSquareClick = {() => handleClick(7)} />
+            <Square key={8} value={squares[8]} onSquareClick = {() => handleClick(8)} />
         </div>
       
       </>
